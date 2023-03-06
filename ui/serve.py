48d0a22b844ddd19baca52606f32e03c9fd34475
builder555 <85308587+builder555@users.noreply.github.com>
2023-03-06 07:58:29 -0500

    fix: serve the right folder

--- ui/serve.py
+++ ui/serve.py
@@ -5,10 +5,9 @@ import os
 PORT = 8080
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
-
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory=DIRECTORY, **kwargs)
+        super().__init__(*args, directory=os.path.join(DIRECTORY,'ui'), **kwargs)
 
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:

8abae604c83114cde7aa20de7cd80ae04db875ca
builder555 <85308587+builder555@users.noreply.github.com>
2023-03-07 14:16:35 -0500

    fix: ensure macos doesn't complain

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
+if [ "$(uname)" == "Darwin" ]; then
+    # for MacOS remove the quarantine attributes
+    # this is needed because the mac will not execute binaries that are downloaded from the internet
+    # see https://developer.apple.com/library/archive/technotes/tn2459/_index.html
+    if xattr -p com.apple.quarantine ./ws_serve/ws_serve >/dev/null 2>&1; then
+        find ./ -type file | xargs xattr -rd com.apple.quarantine
+    fi
+fi
+
 ./ws_serve/ws_serve &
 pid1=$!
 
